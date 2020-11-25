e881e8c558bc2b0f7ae036bc0ea711f53dc91d66
esl4m <me@esl4m.com>
2020-11-26 00:41:54 +0300

    Adds Questions seeder

---
+++ backend/database/seeders/QuestionsSeeder.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Questions;
+use Illuminate\Database\Seeder;
+
+class QuestionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Questions::factory(10)->create();
+        $this->addQuestions();
+    }
+
+    private function addQuestions()
+    {
+        Questions::create(array(
+            'q_num' => 'Q1',
+            'text' => 'You find it takes effort to introduce yourself to other people.', 
+            'dimension' => 'EI',
+            'direction' => '1',
+            'meaning' => 'I',
+            'created_at' => now(),
+            'updated_at' => now()));
+        
+        Questions::create(array(
+            'q_num' => 'Q2',
+            'text' => 'You consider yourself more practical than creative.',  
+            'dimension' => 'SN',
+            'direction' => '-1',
+            'meaning' => 'S',
+            'created_at' => now(),
+            'updated_at' => now()));
+        
+        Questions::create(array(
+            'q_num' => 'Q3',
+            'text' => 'Winning a debate matters less to you than making sure no one gets upset.', 
+            'dimension' => 'TF',
+            'direction' => '1',
+            'meaning' => 'F',
+            'created_at' => now(),
+            'updated_at' => now()));
+            
+        Questions::create(array(
+            'q_num' => 'Q4',
+            'text' => 'You get energized going to social events that involve many interactions.', 
+            'dimension' => 'EI',
+            'direction' => '-1',
+            'meaning' => 'E',
+            'created_at' => now(),
+            'updated_at' => now()));
+        
+        Questions::create(array(
+            'q_num' => 'Q5',
+            'text' => 'You often spend time exploring unrealistic and impractical yet intriguing ideas.', 
+            'dimension' => 'SN',
+            'direction' => '1',
+            'meaning' => 'N',
+            'created_at' => now(),
+            'updated_at' => now()));
+        
+        Questions::create(array(
+            'q_num' => 'Q6',
+            'text' => 'Deadlines seem to you to be of relative rather than absolute importance.', 
+            'dimension' => 'JP',
+            'direction' => '1',
+            'meaning' => 'P',
+            'created_at' => now(),
+            'updated_at' => now()));
+        
+        Questions::create(array(
+            'q_num' => 'Q7',
+            'text' => 'Logic is usually more important than heart when it comes to making important decisions.', 
+            'dimension' => 'TF',
+            'direction' => '-1',
+            'meaning' => 'T',
+            'created_at' => now(),
+            'updated_at' => now()));
+            
+        Questions::create(array(
+            'q_num' => 'Q8',
+            'text' => 'Your home and work environments are quite tidy.', 
+            'dimension' => 'JP',
+            'direction' => '-1',
+            'meaning' => 'J',
+            'created_at' => now(),
+            'updated_at' => now()));
+        
+        Questions::create(array(
+            'q_num' => 'Q9',
+            'text' => 'You do not mind being at the center of attention.', 
+            'dimension' => 'EI',
+            'direction' => '-1',
+            'meaning' => 'E',
+            'created_at' => now(),
+            'updated_at' => now()));
+            
+        Questions::create(array(
+            'q_num' => 'Q10',
+            'text' => 'Keeping your options open is more important than having a to-do list.', 
+            'dimension' => 'JP',
+            'direction' => '1',
+            'meaning' => 'P',
+            'created_at' => now(),
+            'updated_at' => now()));
+    }
+}
