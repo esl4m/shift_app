9962e58c8b187061b81e6a1cea4156280d120c10
esl4m <me@esl4m.com>
2020-11-27 21:31:05 +0300

    Adds routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('questions', 'QuestionsController@index');
+
+Route::group([ 'prefix' => 'questions'], function () {
+    Route::get('/', 'App\Http\Controllers\QuestionsController@index')->name('questions.all');
+    Route::post('/', 'App\Http\Controllers\QuestionsController@store')->name('question.store');
+    Route::post('/save', 'App\Http\Controllers\QuestionsController@save')->name('question.save');
+    Route::post('/delete', 'App\Http\Controllers\QuestionsController@destory')->name('question.destroy');
+});
